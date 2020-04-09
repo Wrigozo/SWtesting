fb1acd4af133d3c0a33fb17485f82dd9c4e34012
Szabó Alexandra <szaboalexandra18@gmail.com>
2020-04-09 13:49:16 +0200

    added homework: regisztráció'

--- src/main/groovy/hu/tigra/pti/geb/page/RegisterPage.groovy
+++ src/main/groovy/hu/tigra/pti/geb/page/RegisterPage.groovy
@@ -2,7 +2,9 @@ package hu.tigra.pti.geb.page
 
 import geb.Page
 import geb.module.RadioButtons
+import geb.module.Checkbox
 import hu.tigra.pti.geb.module.ErrorMessages
+import hu.tigra.pti.geb.module.Date
 
 class RegisterPage extends Page {
 
@@ -16,7 +18,23 @@ class RegisterPage extends Page {
         firstName { $('input[id="customer_firstname"]') }
         lastName { $('input[id="customer_lastname"]') }
         password { $('input[id="passwd"]') }
-        regButton { $('button[id="submitAccount"]') }
+        dateOfBirth { module(Date) }
+        newsletter { $('input[id="newsletter"]').module(Checkbox) }
+        offers { $('input[id="optin"]').module(Checkbox) }
+        registerButton { $('button[id="submitAccount"]') }
         errorMessages(required: false) { $('div[class="alert alert-danger"]:first-of-type').module ErrorMessages }
+        addressFirstName { $('input[id="firstname"]') }
+        addressLastName { $('input[id="lastname"]') }
+        addressCompany { $('input[id="company"]') }
+        addressAddress { $('input[id="address1"]') }
+        addressAddress2 { $('input[id="address2"]') }
+        addressCity { $('input[id="city"]') }
+        addressState { $('select[id="id_state"]') }
+        addressZip { $('input[id="postcode"]') }
+        addressCountry { $('select[id="id_country"]') }
+        addressOther { $('textarea[id="other"]') }
+        addressPhone { $('input[id="phone"]') }
+        addressMobile { $('input[id="phone_mobile"]') }
+        addressAlias { $('input[id="alias"]') }
     }
 }
--- src/test/groovy/hu/tigra/pti/geb/RegisterSpec.groovy
+++ src/test/groovy/hu/tigra/pti/geb/RegisterSpec.groovy
@@ -2,6 +2,7 @@ package hu.tigra.pti.geb
 
 import hu.tigra.pti.geb.page.LoginPage
 import hu.tigra.pti.geb.page.RegisterPage
+import hu.tigra.pti.geb.page.MyAccountPage
 
 class RegisterSpec extends BaseSpec {
 
@@ -10,7 +11,7 @@ class RegisterSpec extends BaseSpec {
         def loginPage = to LoginPage
 
         when: 'Kitöltöm a "CREATE AN ACCOUNT" blokkban az "Email address" mezőt egy még nem regisztrált email címmel és a "Create an account" funkciógombra kattintok.'
-        loginPage.registerEmailAddress = 'teszt@email.hu'
+        loginPage.registerEmailAddress = 'teszt234@email.hu'
         loginPage.registerButton.click()
 
         then: 'Megjelenik a "Create an account" űrlap.'
@@ -21,9 +22,33 @@ class RegisterSpec extends BaseSpec {
         registerPage.firstName = 'Elek'
         registerPage.lastName = 'Teszt'
         registerPage.password = 'teszt123'
-        registerPage.regButton.click()
+        registerPage.dateOfBirth.value('1990', 'May', '12')
+        registerPage.newsletter.check()
+        registerPage.offers.check()
+        registerPage.registerButton.click()
 
         then: '5 hibaüzenet jelenik meg'
         registerPage.errorMessages.values.size() == 5
+
+        //your address-t befejezni mindent kitölteni
+        when: '’Kitöltöm a „Your address” blokkban az összes mezőt és „Register” funkciógombra kattintok'
+        registerPage.password = 'teszt123'
+        registerPage.addressFirstName = 'Elek'
+        registerPage.addressLastName = 'Teszt'
+        registerPage.addressCompany = 'debrői'
+        registerPage.addressAddress = 'kassai 26'
+        registerPage.addressCity = 'Debrecen'
+        registerPage.addressState = 'Arkansas'
+        registerPage.addressZip = '12345'
+        registerPage.addressCountry = 'United States'
+        registerPage.addressOther = 'sssssssss'
+        registerPage.addressPhone = '1234556666'
+        registerPage.addressMobile = '21421412'
+        registerPage.addressAlias = 'aaaaaaaa'
+        registerPage.registerButton.click()
+
+        then: 'Megjelenik a felhasználó adatai felület: “My account”.'
+        def myAccountPage = waitFor { at MyAccountPage }
+        myAccountPage.header.text() == "MY ACCOUNT"
     }
 }
